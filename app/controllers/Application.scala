dabdf74d215a1c5cb769215fb3ddcdb256e762a6
hariDasu <harirao3@gmail.com>
2015-03-16 14:39:49 -0400

    added checkboxes for entry page, adding logic for piping selections

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import models.{Hobbies, Student}
+import models.Student
 
 object Application extends Controller {
 
