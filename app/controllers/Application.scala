e9e2687c23e6c00e84da5777d6088246094aff47
hariDasu <harirao3@gmail.com>
2015-03-15 21:56:14 -0400

    basic form validation, next step bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,27 +4,37 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import models.Customer
 
 object Application extends Controller {
 
+  def customerForm = Form(mapping("Customer Name" -> nonEmptyText, "Credit Limit"->number(min=0,max=100000))(Customer.apply)(Customer.unapply))
+
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(customerForm))
   }
 
-  def doLogin = Action {
-    implicit request => val loginRequest = loginForm.bindFromRequest.get
-      Ok(s"username: '${loginRequest.username}',password: '${loginRequest.password}'")
+  def createCustomer = Action {
+    implicit request => customerForm.bindFromRequest().fold(
+      formWithErrors => BadRequest(views.html.index(formWithErrors)),
+      customer => Ok(s"Customer ${customer.name} created successfully")
+    )
   }
-
-  def loginForm = Form(mapping("username" -> text, "password" -> text)(LoginRequest.apply)(LoginRequest.unapply))
-
-  case class LoginRequest(username:String,password:String)
   /*
-  def handleForm = Action(parse.tolerantFormUrlEncoded) {
-    implicit request =>
-      val username = request.body.get("username").map(_.head).getOrElse("");
-      Ok(views.html.showUserName(username))
-  }*/
+    def doLogin = Action {
+      implicit request => val loginRequest = loginForm.bindFromRequest.get
+        Ok(s"username: '${loginRequest.username}',password: '${loginRequest.password}'")
+    }
+
+    def loginForm = Form(mapping("username" -> text, "password" -> text)(LoginRequest.apply)(LoginRequest.unapply))
+
+    case class LoginRequest(username:String,password:String)
+
+    def handleForm = Action(parse.tolerantFormUrlEncoded) {
+      implicit request =>
+        val username = request.body.get("username").map(_.head).getOrElse("");
+        Ok(views.html.showUserName(username))
+    }*/
 
   //Implicit conversion
   /*
